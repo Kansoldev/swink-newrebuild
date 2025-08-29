6f6bcbde12901ea5750bebbf59ebaeb6327e4566
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2025-08-29 23:26:10 +0100

    Add website font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
 import "../globals.css";
 
 export const metadata: Metadata = {
@@ -6,13 +7,18 @@ export const metadata: Metadata = {
   description: "Unifying Voting and Finance",
 };
 
+const ManropeFont = Manrope({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={ManropeFont.className}>
       <body>{children}</body>
     </html>
   );
