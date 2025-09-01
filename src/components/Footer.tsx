f2e95b0405810ac897483e223d6949a9f9d5b39f
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2025-09-01 17:30:26 +0100

    Change footer social images to svg icons Update swink footer logo as well

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col lg:flex-row items-center lg:items-end justify-between lg:gap-10">
           <div className="w-[1000px] max-w-full h-[300px] relative">
             <Image
-              src="/footer-logo.png"
+              src="/footer-logo.svg"
               className="object-contain"
               alt=""
               fill
@@ -21,28 +21,28 @@ const Footer = () => {
               <Link href="#">Terms & Conditions</Link>
             </div>
 
-            <div className="social-profiles flex mt-8 gap-2">
+            <div className="social-profiles flex mt-8 gap-3">
               <div className="bg-white p-3 rounded-xl flex items-center justify-center">
                 <Link href="">
-                  <Image src="/mail-2.png" width={25} height={25} alt="" />
+                  <Image src="/mail.svg" width={25} height={25} alt="" />
                 </Link>
               </div>
 
               <div className="bg-white p-3 rounded-xl flex items-center justify-center">
                 <Link href="">
-                  <Image src="/x.png" width={25} height={25} alt="" />
+                  <Image src="/x.svg" width={25} height={25} alt="" />
                 </Link>
               </div>
 
               <div className="bg-white p-3 rounded-xl flex items-center justify-center">
                 <Link href="">
-                  <Image src="/telegram.png" width={25} height={25} alt="" />
+                  <Image src="/telegram.svg" width={25} height={25} alt="" />
                 </Link>
               </div>
 
               <div className="bg-white p-3 rounded-xl flex items-center justify-center">
                 <Link href="">
-                  <Image src="/linkedin.png" width={25} height={25} alt="" />
+                  <Image src="/linkedin.svg" width={25} height={25} alt="" />
                 </Link>
               </div>
             </div>
